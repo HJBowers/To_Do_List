d3eff86cd30ee9846c30b4060bb6f44bd27895db
HJBowers <HJBowers@gmail.com>
2018-01-12 17:32:41 -0800

    Started on fetch/AJAX

--- public/script.js
+++ public/script.js
@@ -1,3 +1,21 @@
 'use strict';
 
-document.getElementById('');
+document.getElementById('addTask');
+
+// fetch('/lists/:list_id', {
+//   method: 'POST',
+//   body: JSON.stringify('form data?'),
+//   headers: new Headers({
+//     'Content-Type': 'application/json'
+//   })
+// })
+// .then(res => res.json())
+// .catch(error => console.error('Error:', error))
+// .then(response => console.log('Success:', response));
+
+
+$("button").click(function(){
+    $.ajax({url: "http://localhost:3000/lists/:list_id", success: function(result){
+        $("#addNewTask").json(result);
+    }});
+});
