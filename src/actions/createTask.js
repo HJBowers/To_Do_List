a41ac841401476b0a1f532a467d261bcb3ac9a0b
HJBowers <HJBowers@gmail.com>
2018-01-19 11:07:22 -0800

    Create Task updated

--- src/actions/createTask.js
+++ src/actions/createTask.js
@@ -1,19 +1,10 @@
 import db from '../db'
 
 export default function createTask(task, list_id) {
-  db.none(`
+  return db.none(`
     INSERT INTO
       tasks (task, list_id)
     VALUES
       ($1, $2)
   `, [task, list_id])
-
-  db.any(`
-    SELECT
-      *
-    FROM
-      tasks
-    WHERE
-      list_id=$1
-  `, [list_id])
 }
