934ef832f51a7b07437e5fc1ef7180f88794ac58
HJBowers <HJBowers@gmail.com>
2018-01-24 17:04:34 -0800

    fetch post for new list

--- public/script.js
+++ public/script.js
@@ -1,30 +1,90 @@
-const saveNewTask = function(event) {
+// ********************** Create New Task **********************
+function saveNewTask(event) {
   const newTask = document.getElementById('newTask').value
   const listId = document.getElementById('listId').value
 
   fetch('/addTask', {
     method: 'POST',
-    body: JSON.stringify({ newTask, listId }),
+    body: JSON.stringify({newTask, listId}),
     headers: new Headers({
       'Content-Type': 'application/json'
-    })
+    }),
+    credentials: "include"
   })
-  .then(res => res.json())
+  .then( res => {
+    return res.json()
+  })
   .then( (response) => {
     if(response.status === "Success") {
       $('.tasks').append(`<h3>${newTask}</h3>`)
       $('input[name=newTask]').val('')
     } else {
       console.log("Error Hit!");
-      $('.container').append(`<div><p>There has been an ${response.status}, task has not been added.</p></div>`)
+      $('.container').append(`<div><p>There has been an error, task has not been added.</p></div>`)
     }
   })
   .catch(error => console.error('Error:', error))
 }
 
-document.getElementById("addNewTask").addEventListener('click', function(event) {
-  saveNewTask(event)
+const taskBtn = document.getElementById("addNewTask")
+
+// taskBtn.addEventListener('click', function(event) {
+//   saveNewTask(event)
+//   taskBtn.removeEventListener('click', saveNewTask, false)
+// })
+
+if(taskBtn){
+	taskBtn.onclick = function(){
+		saveNewTask(event)
+	}
+  taskBtn.addEventListener('click', saveNewTask);
+  taskBtn.removeEventListener('click', saveNewTask)
+}
+
+
+
+// ********************** Create New List **********************
+function saveNewList(event) {
+  const title = document.getElementById('newList').value
+  const userId = document.getElementById('userId').value
+  
+  fetch('/lists/createList', {
+    method: 'POST',
+    body: JSON.stringify({title}),
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    }),
+    credentials: "include"
+  })
+  .then( res => {
+    return res.json()
+  })
+  .then( (response) => {
+    if(response.status === 'success') {
+      $('#lists').append(`<h5><a href="http://localhost:3000/lists/${list.id}" >${title} List</a></h5>`)
+      $('input[name=newList]').val('')
+    } else {
+      console.log("Error Hit!")
+      $('#user-list').append(`<div><p>There has been an ${response.statust}, list has not been created.</p></div>`)
+    }
+  })
+  .catch(error => console.error('Error:', error))
+}
+
+var listBtn = document.getElementById('addNewList')
+
+listBtn.addEventListener('click', function(event) {
+  saveNewList(event)
+  listBtn.removeEventListener('click', saveNewList, false)
 })
+
+// if(listBtn){
+// 	listBtn.onclick = function(){
+// 		saveNewList(event)
+// 	}
+//   listBtn.addEventListener('click', saveNewList);
+//   listBtn.removeEventListener('click', saveNewList)
+// }
 
 
 /*
@@ -35,7 +95,7 @@ Steps to get here:
       - url
       - method
       - body includes variables inside an object
-      - promise turn results into json
+      - promise turns results into json
       - promise handles json results
         - if successfull writes to database, it appends to list
         - if fails to write to database, it creates a div with an error message
