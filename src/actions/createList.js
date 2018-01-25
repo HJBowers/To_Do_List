c88953038d1d6aee5b95c958755284b8a74a6a82
HJBowers <HJBowers@gmail.com>
2018-01-25 11:54:15 -0800

    updated query

--- src/actions/createList.js
+++ src/actions/createList.js
@@ -1,10 +1,12 @@
 import db from '../db'
 
 export default function createList(title, user_id) {
-  return db.none(`
+  return db.one(`
     INSERT INTO
       lists (title, user_id)
     VALUES
       ($1, $2)
+    RETURNING
+      *
   `, [title, user_id])
 }
